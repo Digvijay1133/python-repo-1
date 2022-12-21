325104cfc2b48262b56fe0a39a01e12fd3b7b7b9
Digvijay1133 <112845896+Digvijay1133@users.noreply.github.com>
2022-12-21 22:31:41 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 sudo yum -y install python3-pip
 git clone https://github.com/Digvijay1133/Fuel-Consumption-python.git
-cd deploy-python-web-application
+cd Fuel-Consumption-python
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
 

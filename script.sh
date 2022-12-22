674b3f2a9c9a7796c9794934b26c75e4a8dbc716
Digvijay1133 <112845896+Digvijay1133@users.noreply.github.com>
2022-12-22 15:44:56 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -15,3 +15,18 @@ git clone https://github.com/Digvijay1133/USA-House-Pricing-python.git
 cd USA-House-Pricing-python
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
+
+
+sudo yum -y install python3-pip
+git clone https://github.com/Digvijay1133/FISH-WEIGHT-python.git
+cd FISH-WEIGHT-python
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
+
+
+sudo yum -y install python3-pip
+git clone https://github.com/Digvijay1133/LIVER-PATIENT-python.git
+cd LIVER-PATIENT-python
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
+

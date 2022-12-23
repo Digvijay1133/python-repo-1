5652d232683965dab0992f75d764683516eafb32
Digvijay1133 <112845896+Digvijay1133@users.noreply.github.com>
2022-12-23 11:08:24 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,27 +4,27 @@ cd Fuel-Consumption-python
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
 
-sudo yum -y install python3-pip
+
 git clone https://github.com/Digvijay1133/agri-python.git
 cd agri-python
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
 
-sudo yum -y install python3-pip
+
 git clone https://github.com/Digvijay1133/USA-House-Pricing-python.git
 cd USA-House-Pricing-python
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
 
 
-sudo yum -y install python3-pip
+
 git clone https://github.com/Digvijay1133/FISH-WEIGHT-python.git
 cd FISH-WEIGHT-python
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
 
 
-sudo yum -y install python3-pip
+
 git clone https://github.com/Digvijay1133/LIVER-PATIENT-python.git
 cd LIVER-PATIENT-python
 pip3 install -r requirements.txt
